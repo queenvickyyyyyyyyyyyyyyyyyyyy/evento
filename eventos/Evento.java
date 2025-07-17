0445c279d8a6a0f651b97377b80eb64174902d1e
Vitória Martins <133159073+queenvickyyyyyyyyyyyyyyyyyyyy@users.noreply.github.com>
2025-07-17 11:45:00 -0300

    Update Evento.java

--- eventos/Evento.java
+++ eventos/Evento.java
@@ -9,39 +9,46 @@ public class Evento {
     private int capacidadeMaxima;
 
 
-public Evento(String nome, LocalDate data, int capacidadeMaxima){
-    this.nome = nome;
-    this.data = data;
-    this.capacidadeMaxima = capacidadeMaxima;
-    this.participantes = new HashSet<>();
-}
-
-public String getNome(){
-    return nome;
-}
-
-public LocalDate getData(){
-    return data;
-}
-
-public int getCapacidadeMaxima(){
-    return capacidadeMaxima;
-}
-
-public Set<Participante> getParticipantes(){
-    return participantes;
-}
-
-public boolean adicionarParticipante(Participante p){
-    if (participantes.size() < capacidadeMaxima){
-        return participantes.add(p);
+    public Evento(String nome, LocalDate data, int capacidadeMaxima){
+        this.nome = nome;
+        this.data = data;
+        this.capacidadeMaxima = capacidadeMaxima;
+        this.participantes = new HashSet<>();
     }
-    return false;
-}
 
-public List<Participante> buscarParticipantesPorNome(String nome){
-    return participantes.stream()
-            .sorted(Comparator.comparing(null))
+    public String getNome(){
+        return nome;
+    }
 
-}
+    public LocalDate getData(){
+        return data;
+    }
+
+    public int getCapacidadeMaxima(){
+        return capacidadeMaxima;
+    }
+
+    public Set<Participante> getParticipantes(){
+        return participantes;
+    }
+
+    public boolean adicionarParticipante(Participante p){
+        if (participantes.size() < capacidadeMaxima){
+            return participantes.add(p);
+        }
+        return false;
+    }
+
+    public List<Participante> listarParticipantesPorNome(){
+        return participantes.stream()
+                .sorted(Comparator.comparing(Participante::getNome))
+                .collect(Collectors.toList());
+    }
+
+    public Set<Participante> buscarParticipantesPorInicial(char letra){
+        return participantes.stream()
+                .filter(p -> p.getNome().toUpperCase().charAt(0) == Character.toUpperCase(letra))
+                .collect(Collectors.toSet());
+    }
+
 }
