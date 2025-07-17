2d5c44dd1611b5d6389debf03a8d74208460329f
Vitória Martins <133159073+queenvickyyyyyyyyyyyyyyyyyyyy@users.noreply.github.com>
2025-07-17 11:45:19 -0300

    Update SistemaEventos.java

--- eventos/SistemaEventos.java
+++ eventos/SistemaEventos.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SistemaEventos{
+    public Map<String, Evento> eventos = new HashMap<>();
+
+    public void adicionarEvento(Evento evento){
+        if(evento != null){
+            eventos.put(evento.getNome(), evento);
+        }
+    }
+
+    public Evento buscarEvento(String nome){
+        return eventos.get(nome);
+    }
+
+    public List<Evento> listarEventos(){
+        return new ArrayList<>(eventos.values());
+    }
     
 }
